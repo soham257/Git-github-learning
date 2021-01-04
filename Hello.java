33f72c8db95ea30a595c099627e800a3b6887cd2
Soham <sohampatil257@gmail.com>
2021-01-04 12:07:36 +0530

    Added For loop

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 	Public static void main(String[] args){
-		System.out.println("Hello World!");
-		System.out.println("Hello");
+		for(int i=1; i<=5; i++){
+			System.out.println("Hello World!")
+		}
 	}
 }
